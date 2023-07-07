8fb0c891d4b9cb97e1fb26f6bf53396b7494ed05
vassilis <vassilisminadakis@gmail.com>
2023-07-07 16:25:49 +0300

    Update goodness_of_fit_metrics.R

--- R/goodness_of_fit_metrics.R
+++ R/goodness_of_fit_metrics.R
@@ -79,3 +79,58 @@ PBKOF <- function(observed, predicted, comp.names =NULL){
   return(Ic)
   #return(list(Total_index = Ic, Compartment_index= I))
 }
+
+#=====================================#
+#  Weighted Sum of Squared Residuals  #
+#=====================================#
+
+WSSR <- function(observed, predicted, weights, comp.names =NULL){
+  # Check if the user provided the correct input format
+  if (!is.list(observed) || !is.list(predicted) || !is.list(weights)){
+    stop(" The observations, predictions and weights must be lists")
+  }
+  # Check if the user provided equal length lists
+  if (length(observed) != length(predicted) || length(observed) != length(weights)){
+    stop(" The observations, predictions and weights must have the same compartments")
+  }
+  
+  # Define the number of observed outputs
+  N_outputs <- length(predicted)
+  # Define the number of observations per output
+  N_obs <- rep(NA, N_outputs)
+  
+  # A vector to store the values of the weighted squared sum residuals of each compartment
+  outputs_res <- c()
+  for (i in 1:N_outputs) { # loop over the observed outputs
+    N_obs[i] <- length(observed[[i]])
+    
+    # Check that all observed, predicted and weights vectors have the same length
+    if(N_obs[i] != length(predicted[[i]]) || N_obs[i] != length(weights[[i]])){
+      stop(paste0("Compartment ",i," had different length in the observations and predictions"))
+    }
+    # The number of observations for output i
+    N <- N_obs[i]
+    
+    # Initiate a variable to estimate the sum of squared residuals for output j
+    sq_weighted_res_j <- 0
+    for (j in 1:N) { #loop over the experimental points i compartment i
+      sq_weighted_res_j <- sq_weighted_res_j + ((observed[[i]][j] - predicted[[i]][j]) / weights[[i]][j])^2   
+    }
+    outputs_res[i] <- sq_weighted_res_j
+  }
+  
+  WSSR_results <- sum(outputs_res)
+  
+  # Name the list of compartment discrepancy indices
+  if ( !is.null(comp.names)){
+    names(WSSR_results) <- comp.names
+  }else if (!is.null(names(observed))){
+    names(WSSR_results) <- names(observed)
+  } else if (!is.null(names(predicted)) && is.null(comp.names) ){
+    names(WSSR_results) <- names(predicted)
+  } else if (!is.null(names(weights)) && is.null(comp.names) ){
+    names(WSSR_results) <- names(weights)
+  }
+  
+  return(WSSR_results)
+}
